d3abac271ae1a25b581d24d90df05626cb65191d
Somik44 <Somik.228@yandex.ru>
2025-12-13 16:24:13 +0300

    добавил движение

--- Factorio/Factorio/MainWindow.xaml.cs
+++ Factorio/Factorio/MainWindow.xaml.cs
@@ -1,22 +1,225 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Factorio
 {
-
     public partial class MainWindow : Window
     {
+        private string[] texturePaths = new string[]
+        {
+            @"C:\Users\Михаил\Desktop\Game\Factorio\Factorio\textures\earth2.jpg",
+            @"C:\Users\Михаил\Desktop\Game\Factorio\Factorio\textures\earth3.jpg",
+            @"C:\Users\Михаил\Desktop\Game\Factorio\Factorio\textures\earth4.jpg"
+        };
+
+        private int mapWidth = 6;
+        private int mapHeight = 6;
+        private Player player;
+        private DispatcherTimer gameLoopTimer;
+
+        // Состояние нажатых клавиш
+        private bool isUpPressed = false;
+        private bool isDownPressed = false;
+        private bool isLeftPressed = false;
+        private bool isRightPressed = false;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Left = SystemParameters.WorkArea.Left;
+            this.Top = SystemParameters.WorkArea.Top;
+            this.Width = SystemParameters.WorkArea.Width;
+            this.Height = SystemParameters.WorkArea.Height;
+
+            CreateTileMapToFillWindow();
+            InitializePlayer();
+            InitializeGameLoop();
+
+            this.Focus();
+        }
+
+        private void CreateTileMapToFillWindow()
+        {
+            GameCanvas.Children.Clear();
+
+            double canvasWidth = this.ActualWidth;
+            double canvasHeight = this.ActualHeight;
+
+            double tileWidth = canvasWidth / mapWidth;
+            double tileHeight = canvasHeight / mapHeight;
+
+            Random random = new Random();
+
+            for (int row = 0; row < mapHeight; row++)
+            {
+                for (int col = 0; col < mapWidth; col++)
+                {
+                    int textureIndex = random.Next(texturePaths.Length);
+
+                    Image tile = new Image
+                    {
+                        Width = tileWidth,
+                        Height = tileHeight,
+                        Source = new BitmapImage(new Uri(texturePaths[textureIndex])),
+                        Stretch = Stretch.UniformToFill
+                    };
+
+                    Canvas.SetLeft(tile, col * tileWidth);
+                    Canvas.SetTop(tile, row * tileHeight);
+
+                    GameCanvas.Children.Add(tile);
+                }
+            }
+        }
+
+        private void InitializePlayer()
+        {
+            double startX = this.ActualWidth / 2 - 25;
+            double startY = this.ActualHeight / 2 - 25;
+
+            player = new Player(startX, startY, 50, 50);
+            player.AddToCanvas(GameCanvas);
+        }
+
+        private void InitializeGameLoop()
+        {
+            gameLoopTimer = new DispatcherTimer();
+            gameLoopTimer.Interval = TimeSpan.FromMilliseconds(16); // ~60 FPS
+            gameLoopTimer.Tick += GameLoop_Tick;
+            gameLoopTimer.Start();
+        }
+
+        private void GameLoop_Tick(object sender, EventArgs e)
+        {
+            UpdatePlayerMovement();
+            player.UpdateAnimation();
+        }
+
+        private void UpdatePlayerMovement()
+        {
+            double deltaX = 0;
+            double deltaY = 0;
+            Direction direction = Direction.Down;
+
+            // Определяем направление движения
+            if (isUpPressed && !isDownPressed)
+            {
+                deltaY = -1;
+                direction = Direction.Up;
+            }
+            else if (isDownPressed && !isUpPressed)
+            {
+                deltaY = 1;
+                direction = Direction.Down;
+            }
+
+            if (isLeftPressed && !isRightPressed)
+            {
+                deltaX = -1;
+                direction = Direction.Left;
+            }
+            else if (isRightPressed && !isLeftPressed)
+            {
+                deltaX = 1;
+                direction = Direction.Right;
+            }
+
+            // Комбинированное движение по диагонали
+            if (Math.Abs(deltaX) > 0 && Math.Abs(deltaY) > 0)
+            {
+                // Нормализуем вектор для диагонального движения
+                double length = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+                deltaX /= length;
+                deltaY /= length;
+
+                // Для диагонали используем горизонтальное направление
+                if (deltaX < 0) direction = Direction.Left;
+                else if (deltaX > 0) direction = Direction.Right;
+            }
+
+            // Если есть движение
+            if (deltaX != 0 || deltaY != 0)
+            {
+                player.Move(deltaX, deltaY, direction);
+            }
+            else
+            {
+                player.Stop();
+            }
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            switch (e.Key)
+            {
+                case Key.Escape:
+                    this.Close();
+                    break;
+
+                case Key.W:
+                case Key.Up:
+                    isUpPressed = true;
+                    break;
+
+                case Key.S:
+                case Key.Down:
+                    isDownPressed = true;
+                    break;
+
+                case Key.A:
+                case Key.Left:
+                    isLeftPressed = true;
+                    break;
+
+                case Key.D:
+                case Key.Right:
+                    isRightPressed = true;
+                    break;
+            }
+        }
+
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            base.OnKeyUp(e);
+
+            switch (e.Key)
+            {
+                case Key.W:
+                case Key.Up:
+                    isUpPressed = false;
+                    break;
+
+                case Key.S:
+                case Key.Down:
+                    isDownPressed = false;
+                    break;
+
+                case Key.A:
+                case Key.Left:
+                    isLeftPressed = false;
+                    break;
+
+                case Key.D:
+                case Key.Right:
+                    isRightPressed = false;
+                    break;
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
---
+++ Factorio/Factorio/Player.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Collections.Generic;
+using System.Windows;
+using System.IO;
+
+namespace Factorio
+{
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    public class Player
+    {
+        // Свойства персонажа
+        public Image Sprite { get; private set; }
+        public double X { get; private set; }
+        public double Y { get; private set; }
+        public double Width { get; private set; }
+        public double Height { get; private set; }
+        public double Speed { get; set; }
+        public Direction CurrentDirection { get; private set; }
+
+        // Анимации
+        private Dictionary<Direction, List<BitmapImage>> animations;
+        private int currentFrame = 0;
+        private int animationSpeed = 150; // мс между кадрами
+        private DateTime lastFrameTime;
+
+        // Состояние движения
+        public bool IsMoving { get; private set; }
+
+        public Player(double startX, double startY, double width, double height)
+        {
+            X = startX;
+            Y = startY;
+            Width = width;
+            Height = height;
+            Speed = 3.0;
+            CurrentDirection = Direction.Down;
+            lastFrameTime = DateTime.Now;
+
+            InitializeSprite();
+            LoadAnimations();
+        }
+
+        private void InitializeSprite()
+        {
+            Sprite = new Image
+            {
+                Width = Width,
+                Height = Height,
+                Stretch = System.Windows.Media.Stretch.Uniform
+            };
+
+            UpdatePosition();
+        }
+
+        private void LoadAnimations()
+        {
+            animations = new Dictionary<Direction, List<BitmapImage>>();
+            string basePath = @"C:\Users\Михаил\Desktop\Game\Factorio\Factorio\textures\Player\";
+
+            // Создаем списки для каждого направления
+            animations[Direction.Up] = new List<BitmapImage>();
+            animations[Direction.Down] = new List<BitmapImage>();
+            animations[Direction.Left] = new List<BitmapImage>();
+            animations[Direction.Right] = new List<BitmapImage>();
+
+            // Загружаем спрайты для каждого направления
+            LoadDirectionSprites(Direction.Up, basePath);
+            LoadDirectionSprites(Direction.Down, basePath);
+            LoadDirectionSprites(Direction.Left, basePath);
+            LoadDirectionSprites(Direction.Right, basePath);
+        }
+
+        private void LoadDirectionSprites(Direction direction, string basePath)
+        {
+            string directionName = direction.ToString();
+
+            // Сначала пытаемся загрузить спрайт стояния
+            string standingPath = Path.Combine(basePath, $"{directionName}.png");
+            if (File.Exists(standingPath))
+            {
+                animations[direction].Add(new BitmapImage(new Uri(standingPath)));
+            }
+            else
+            {
+                // Создаем заглушку для стояния
+                animations[direction].Add(CreatePlaceholderSprite(direction, "S"));
+            }
+
+            // Затем загружаем спрайты движения
+            for (int i = 1; i <= 2; i++)
+            {
+                string movePath = Path.Combine(basePath, $"{directionName}_move({i}).png");
+                if (File.Exists(movePath))
+                {
+                    animations[direction].Add(new BitmapImage(new Uri(movePath)));
+                }
+                else
+                {
+                    // Создаем заглушку для движения
+                    animations[direction].Add(CreatePlaceholderSprite(direction, $"M{i}"));
+                }
+            }
+        }
+
+        private BitmapImage CreatePlaceholderSprite(Direction direction, string label = "S")
+        {
+            // Создание простого спрайта-заглушки
+            var renderTarget = new System.Windows.Media.Imaging.RenderTargetBitmap(
+                (int)Width, (int)Height, 96, 96, System.Windows.Media.PixelFormats.Pbgra32);
+
+            var drawingVisual = new System.Windows.Media.DrawingVisual();
+
+            using (var drawingContext = drawingVisual.RenderOpen())
+            {
+                // Разные цвета для разных направлений
+                System.Windows.Media.Brush color = direction switch
+                {
+                    Direction.Up => System.Windows.Media.Brushes.Blue,
+                    Direction.Down => System.Windows.Media.Brushes.Green,
+                    Direction.Left => System.Windows.Media.Brushes.Yellow,
+                    Direction.Right => System.Windows.Media.Brushes.Red,
+                    _ => System.Windows.Media.Brushes.White
+                };
+
+                // Фон
+                drawingContext.DrawRectangle(color, null,
+                    new Rect(0, 0, Width, Height));
+
+                // Черная рамка
+                drawingContext.DrawRectangle(
+                    System.Windows.Media.Brushes.Black,
+                    new System.Windows.Media.Pen(System.Windows.Media.Brushes.Black, 2),
+                    new Rect(0, 0, Width, Height));
+
+                // Текст с направлением и типом спрайта
+                string text = $"{direction.ToString()[0]}{label}";
+                drawingContext.DrawText(
+                    new System.Windows.Media.FormattedText(
+                        text,
+                        System.Globalization.CultureInfo.CurrentCulture,
+                        System.Windows.FlowDirection.LeftToRight,
+                        new System.Windows.Media.Typeface("Arial"),
+                        14,
+                        System.Windows.Media.Brushes.Black),
+                    new Point(Width / 2 - 15, Height / 2 - 7));
+            }
+
+            renderTarget.Render(drawingVisual);
+            var bitmap = new BitmapImage();
+            var encoder = new System.Windows.Media.Imaging.PngBitmapEncoder();
+            encoder.Frames.Add(System.Windows.Media.Imaging.BitmapFrame.Create(renderTarget));
+
+            using (var stream = new System.IO.MemoryStream())
+            {
+                encoder.Save(stream);
+                stream.Seek(0, System.IO.SeekOrigin.Begin);
+                bitmap.BeginInit();
+                bitmap.CacheOption = System.Windows.Media.Imaging.BitmapCacheOption.OnLoad;
+                bitmap.StreamSource = stream;
+                bitmap.EndInit();
+            }
+
+            return bitmap;
+        }
+
+        public void Move(double deltaX, double deltaY, Direction direction)
+        {
+            IsMoving = true;
+            CurrentDirection = direction;
+
+            X += deltaX * Speed;
+            Y += deltaY * Speed;
+
+            // Ограничиваем перемещение в пределах экрана
+            X = Math.Max(0, Math.Min(X, SystemParameters.PrimaryScreenWidth - Width));
+            Y = Math.Max(0, Math.Min(Y, SystemParameters.PrimaryScreenHeight - Height));
+
+            UpdatePosition();
+        }
+
+        public void Stop()
+        {
+            IsMoving = false;
+            currentFrame = 0; // Возвращаемся к первому кадру (стояние)
+        }
+
+        public void UpdateAnimation()
+        {
+            if (IsMoving && animations.ContainsKey(CurrentDirection) && animations[CurrentDirection].Count > 0)
+            {
+                // Проверяем время для смены кадра
+                if ((DateTime.Now - lastFrameTime).TotalMilliseconds >= animationSpeed)
+                {
+                    // При движении переключаемся между кадрами 1 и 2 (движение)
+                    // Кадр 0 - стояние
+                    if (currentFrame == 0)
+                        currentFrame = 1;
+                    else
+                        currentFrame = (currentFrame == 1) ? 2 : 1;
+
+                    lastFrameTime = DateTime.Now;
+                }
+
+                // Устанавливаем текущий спрайт
+                Sprite.Source = animations[CurrentDirection][currentFrame];
+            }
+            else if (animations.ContainsKey(CurrentDirection) && animations[CurrentDirection].Count > 0)
+            {
+                // Если персонаж не двигается, показываем первый кадр (стояние)
+                currentFrame = 0;
+                Sprite.Source = animations[CurrentDirection][0];
+            }
+        }
+
+        private void UpdatePosition()
+        {
+            Canvas.SetLeft(Sprite, X);
+            Canvas.SetTop(Sprite, Y);
+        }
+
+        public void AddToCanvas(Canvas canvas)
+        {
+            if (!canvas.Children.Contains(Sprite))
+            {
+                canvas.Children.Add(Sprite);
+                UpdatePosition();
+            }
+        }
+
+        public void RemoveFromCanvas(Canvas canvas)
+        {
+            if (canvas.Children.Contains(Sprite))
+            {
+                canvas.Children.Remove(Sprite);
+            }
+        }
+    }
+}
